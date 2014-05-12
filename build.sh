492ae97d051e1b3d731227838b815cee00ce485f
jannishuebl <jannis.huebl@web.de>
2014-05-12 18:08:12 +0200

    create all output formats

--- build.sh
+++ build.sh
@@ -2,3 +2,14 @@ pdflatex -output-directory=out index.tex
 pdflatex -output-directory=out index.tex
 pdflatex -output-directory=out index.tex
 
+pdflatex -output-directory=out handout_raw.tex
+pdflatex -output-directory=out handout_raw.tex
+pdflatex -output-directory=out handout_raw.tex
+
+pdflatex -output-directory=out handout.tex
+pdflatex -output-directory=out handout.tex
+pdflatex -output-directory=out handout.tex
+
+pdflatex -output-directory=out article.tex
+pdflatex -output-directory=out article.tex
+pdflatex -output-directory=out article.tex
